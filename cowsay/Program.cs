885a3e5d951cdefe6637532cba2afd6ba8959b98
Terry Trent <terry.trent@gmail.com>
2016-08-09 17:58:18 -0400

    initial commit

---
+++ cowsay/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cowsay
+{
+    class program
+    {
+        static void Main(string[] args)
+         {
+            string message = "We begin this tale, with me having left [Telco] to work at a [Data Center] for more money. While I had IT certifications at this point, the [Data Center] gave me the chance to actually put them to use, while also keeping my telco skills alive.";
+
+            SpeechBubble Speak = new SpeechBubble(message,null);
+            cow Cow = new cow();
+            Cow.Tongue = "U";
+            Cow.showCow();
+            //cowsay newCow = new cowsay(message);
+            
+        }
+    }
+}
---
+++ cowsay/cow.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cowsay
+{
+    class cow
+    {
+        private string eyes;
+        private string tongue;
+        private string cowAscii;
+
+        public string CowAscii
+        {
+            get { return cowAscii; }
+            set { cowAscii = value; }
+        }
+
+
+        public string Eyes
+        {
+            get { return eyes; }
+            set
+            {
+                if (value != null)
+                {
+                    int eyesLength = value.Length;
+                    if (eyesLength != 2)
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(Eyes), "Must be exactly 2 characters in length");
+                    }
+                }
+                eyes = value;
+            }
+        }
+
+        public string Tongue
+        {
+            get { return tongue; }
+            set
+            {
+                if (value != null)
+                {
+                    int tongueLength = value.Length;
+                    if (tongueLength > 2)
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(Eyes), "Must be 1 or 2 characters in length");
+                    }
+
+                    if (tongueLength != 2)
+                    {
+                        tongue = $"a {value}";
+                    }
+                }
+                tongue = value; }
+        }
+
+        public cow() : this(null, null)
+        {
+
+        }
+
+        public cow(string cowEyes, string cowTongue)
+        {
+            
+            
+
+            if (Eyes == null)
+                cowEyes = "oo";
+            if (Tongue == null)
+                cowTongue = "  ";
+                
+            Eyes = cowEyes;
+            Tongue = cowTongue;
+
+            CowAscii = $@"         \   ^__^ 
+          \  ({Eyes})\_______
+             (__)\       )\/\
+              {Tongue} ||----w |
+                 ||     ||";
+        }
+
+        public void showCow()
+        {
+            Console.WriteLine(CowAscii);
+        }
+    }
+}
