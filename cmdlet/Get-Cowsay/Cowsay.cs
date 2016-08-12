78eaa36ca3f9e59780a706d481ff8d6e82e03e28
Terry Trent <terry.trent@gmail.com>
2016-08-12 17:56:58 -0400

    began on cowsay cmdlet

---
+++ cmdlet/Get-Cowsay/Cowsay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using CowsaySharp.Library;
+
+namespace CowsaySharp.GetCowsay.Containers
+{
+    class Cowsay
+    {
+        public SpeechBubble speechBubble { get; set; }
+        public string Cow { get; set; }
+        public string SpeechBubbleAndCow { get; set; }
+
+        public Cowsay(string cow, string speech)
+        {
+            speechBubble = new SpeechBubble(speech);
+
+            Cow = "cowFile";
+            SpeechBubbleAndCow = $"{speechBubble}{Environment.NewLine}{Cow}";
+        }
+    }
+}
---
+++ cmdlet/Get-Cowsay/GetCowsayCmdlet.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using CowsaySharp.GetCowsay.Containers;
+using CowsaySharp.Library;
+using CowsaySharp.Common;
+
+//https://www.simple-talk.com/dotnet/net-development/using-c-to-create-powershell-cmdlets-the-basics/
+
+namespace CowsaySharp.GetCowsay
+{
+    [Cmdlet(VerbsCommon.Get, "Cowsay")]
+    [OutputType(typeof(Cowsay))]
+    public class GetCowsayCmdlet : Cmdlet
+    {
+        [Parameter]
+        [ValidateSet("borg","dead","greedy","paranoid","stoned","tired","wired","young")]
+        [Alias("m")]
+        public string mode { get; set; }
+
+        [Parameter]
+        [Alias("l")]
+        public SwitchParameter list { get; set; }
+
+        [Parameter]
+        [Alias("n")]
+        public SwitchParameter figlet { get; set; }
+
+        [Parameter]
+        [Alias("h")]
+        public SwitchParameter help { get; set; }
+
+        [Parameter]
+        [Alias("e")]
+        public string eyes { get; set; }
+
+        [Parameter]
+        [Alias("T")]
+        public string tongue { get; set; }
+
+        [Parameter]
+        [Alias("f")]
+        public string cowfile { get; set; }
+
+        [Parameter]
+        [Alias("W")]
+        public int wrapcolumn { get; set; }
+
+        [Parameter(ValueFromPipeline = true,Mandatory = true,ValueFromRemainingArguments = true)]
+        public string message { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            //setup variables based on switches
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            //generate cow and assign to cowsay container to return
+            //base.ProcessRecord();
+            WriteObject(BuildOutputObject());
+           
+        }
+
+        private Cowsay BuildOutputObject()
+        {
+            return new Cowsay("cow", message);
+            {
+
+            };
+        }
+    }
+}
+
